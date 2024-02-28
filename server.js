8719819e4e2fbd8345d8a8a6cf7c2ef2dab1910c
civteam-rgreenawalt <rgreenawalt@civteam.com>
2024-02-28 14:48:57 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,7 @@
 const app = require('./app');
 const PORT = 3073;
-// const mongoose = require('mongoose');
-// const { dbConn, mongooseOptions } = require('./config/mongodb');
-
 
 console.log('attempting to connect to MongoDB...');
-
-// mongoose.connect(dbConn, mongooseOptions)
-//     .then(res => {
-//         console.log('✅ connected to MongoDB!');
-
-//         app.listen(PORT, () => {
-//             console.log('-----------------------------------');
-//             console.log('App is listening on port ', PORT);
-//             console.log(`view the app ====> http://localhost:${PORT}`);
-//             console.log('-----------------------------------');
-//         });
-//     })
-//     .catch(err => {
-//         console.log('Could not connect to mongodb', err);
-//         process.exit(1);
-//     })
-
 
 app.listen(PORT, () => {
     console.log('-----------------------------------');
