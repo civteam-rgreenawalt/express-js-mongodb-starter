91dd7ede5f4036be9e2a4eb4deeaf22da12dda75
civteam-rgreenawalt <rgreenawalt@civteam.com>
2024-02-28 15:03:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const app = require('./app');
 const PORT = 3073;
 
-console.log('attempting to connect to MongoDB...');
-
 app.listen(PORT, () => {
     console.log('-----------------------------------');
     console.log('App is listening on port ', PORT);
