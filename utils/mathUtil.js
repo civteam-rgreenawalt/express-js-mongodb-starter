84e690d8db1bebac5b88e8b7a3719722bed9bcf2
Robert Greenawalt, Jr <rgreenawalt@civteam.com>
2025-06-23 15:02:55 -0400

    Add mathUtil

---
+++ utils/mathUtil.js
@@ -0,0 +1,34 @@
+const mathUtil = {
+
+    sum(a, b) {
+        return a + b;
+    },
+
+    subtract(a, b) {
+        return a - b;
+    },
+
+    multiply(a, b) {
+        return a * b;
+    },
+
+    divide(a, b) {
+        if (b === 0) {
+            throw new Error("Division by zero is not allowed.");
+        }
+        return a / b;
+    },
+
+    power(base, exponent) {
+        return Math.pow(base, exponent);
+    },
+
+    sqrt(a) {
+        if (a < 0) {
+            throw new Error("Square root of negative number is not allowed.");
+        }
+        return Math.sqrt(a);
+    }
+}
+
+module.exports = mathUtil;
